afde62ff92e35f0f51bc58f8a1fee4e38273de26
Learn14-know <shriyaarora97@gmail.com>
2025-12-04 15:43:51 -0500

    Update main.tf

--- Modules/aks/main.tf
+++ Modules/aks/main.tf
@@ -1,25 +1,34 @@
-resource "azurerm_resource_group" "this" {
-  name     = var.resource_group_name
-  location = var.location
-}
+variable "name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "kubernetes_version" { default = "1.28.0" }
+variable "node_count" { default = 2 }
+variable "node_vm_size" { default = "Standard_D2s_v3" }
 
-resource "azurerm_kubernetes_cluster" "this" {
-  name                = var.aks_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  dns_prefix          = var.dns_prefix
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.name
 
   default_node_pool {
-    name       = var.node_pool_name
+    name       = "agentpool"
     node_count = var.node_count
-    vm_size    = var.vm_size
+    vm_size    = var.node_vm_size
+  }
+
+  role_based_access_control {
+    enabled = true
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  tags = {
-    Environment = var.environment
-  }
+  # networking, addon_profiles etc as inputs
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+  sensitive = true
 }
