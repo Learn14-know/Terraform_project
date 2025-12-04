ca708679edf21fbe0b33e4128005e95593ccc250
Learn14-know <shriyaarora97@gmail.com>
2025-12-04 15:40:59 -0500

    Update backend.tf

--- Environments/dev/backend.tf
+++ Environments/dev/backend.tf
@@ -1,8 +1,17 @@
+# environments/dev/backend.tf
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+
   backend "azurerm" {
-    resource_group_name  = "RG"
+    resource_group_name  = "tfstate-rg"
     storage_account_name = "shreyastorage123456789"
-    container_name       = "terracontainer"
-    key                  = "dev.tfstate"   # change to stage.tfstate in stage folder
+    container_name       = "container1"
+    key                  = "dev.tfstate"
   }
 }
+
