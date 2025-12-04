d663efeb21d63f8813c25cd3b6d213e1fb714c9e
Learn14-know <shriyaarora97@gmail.com>
2025-12-04 15:45:31 -0500

    Update main.tf

--- Environments/dev/main.tf
+++ Environments/dev/main.tf
@@ -1,28 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  env = "dev"
+}
+
 module "aks" {
   source = "../../modules/aks"
-
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  aks_name            = var.aks_name
-  dns_prefix          = var.dns_prefix
-  node_count          = var.node_count
-  vm_size             = var.vm_size
-  environment         = "dev"  # fixed for this environment
-  keyvault_id         = module.keyvault.keyvault_id  # pass Key Vault reference
+  name = "aks-${local.env}"
+  location = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  node_count = var.node_count
 }
 
-module "keyvault" {
-  source              = "../../modules/keyvault"
-  keyvault_name       = "kv"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  tenant_id           = var.tenant_id
-  spn_client_id       = var.spn_client_id
-  spn_client_secret   = var.spn_client_secret
-  spn_object_id       = var.spn_object_id
-  admin_object_id     = var.admin_object_id
-  tags = {
-    Environment = "dev"
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
-
